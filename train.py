c4d62c83d214f17e50588032add1628aa3007f6c
ds-learning-2020 <63844954+ds-learning-2020@users.noreply.github.com>
2020-10-21 19:42:08 +0530

    add train.py

---
+++ train.py
@@ -0,0 +1,43 @@
+from sklearn.linear_model import LogisticRegression
+import argparse
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+
+df = TabularDatasetFactory.from_delimited_files("Corona_NLP_train.csv").to_pandas_dataframe(encoding='latin1')
+
+
+run = Run.get_context()
+    
+y = df["Sentiment"]
+del df["Sentiment"]
+
+x_train, x_test , y_train, y_test = train_test_split(df, y, test_size=20, random_state=42)
+
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+    print("test")
+
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
+
+if __name__ == '__main__':
+    main()
